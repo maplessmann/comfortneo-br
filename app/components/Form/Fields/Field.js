0966785b620ac6014c19d0d37c014ed504207ed0
Matheus Plessmann <maplessmann@gmail.com>
2020-01-13 16:49:01 -0300

    Adjust page style

--- app/components/Form/Fields/Field.js
+++ app/components/Form/Fields/Field.js
@@ -1,12 +1,18 @@
 import { Field, ErrorMessage } from 'formik'
 
-const Text = ({ name, type = 'text', label, ...props }) => {
+const Text = ({ name, type = 'text', label, component }) => {
   return (
     <div className="field-group">
       <label htmlFor={name} className="field-label">
         {label}
       </label>
-      <Field className="field" id={name} type={type} name={name} {...props} />
+      <Field
+        className="field"
+        id={name}
+        type={type}
+        name={name}
+        component={component}
+      />
       <ErrorMessage className="field-error" component="span" name={name} />
     </div>
   )
--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -1,9 +1,11 @@
 import PageWrapper from 'components/PageWrapper'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <PageWrapper className="home-page">
-      <h1>Tradução confortneo</h1>
+      <h1 className="title">Tradução comfortneo</h1>
+      <Link to="/avaliar" className="button">Fazer avaliação</Link>
     </PageWrapper>
   )
 }
